a3c25a2c27666d68e46315e3aeb0289e4c856414
AzurIce <973562770@qq.com>
2025-04-28 12:08:17 +0800

    chore: added flake.nix

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "Hello world flake using uv2nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs) lib;
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust-tools = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+
+        # Use Python 3.13 from nixpkgs
+        python = pkgs.python313;
+      in {
+        # This example provides two different modes of development:
+        # - Impurely using uv to manage virtual environments
+        # - Pure development using uv2nix to manage virtual environments
+        devShells.default =
+          # It is of course perfectly OK to keep using an impure virtualenv workflow and only use uv2nix to build packages.
+          # This devShell simply adds Python and undoes the dependency leakage done by Nixpkgs Python infrastructure.
+          pkgs.mkShell {
+            packages = with pkgs;
+              [
+                # clang
+                # llvmPackages_17.bintools
+                # libusb1
+                # openssl
+                pkg-config
+                uv
+                cairo
+                portaudio
+              ] ++ [ python rust-tools ]
+              ++ (with pkgs.darwin.apple_sdk.frameworks;
+                pkgs.lib.optionals pkgs.stdenv.isDarwin [
+                  System
+                  IOKit
+                  Security
+                  CoreFoundation
+                  AppKit
+                ]);
+
+            env = {
+              # Prevent uv from managing Python downloads
+              UV_PYTHON_DOWNLOADS = "never";
+              # Force uv to use nixpkgs Python interpreter
+              UV_PYTHON = python.interpreter;
+            } // lib.optionalAttrs pkgs.stdenv.isLinux {
+              # Python libraries often load native shared objects using dlopen(3).
+              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+              LD_LIBRARY_PATH =
+                lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
+            shellHook = ''
+              unset PYTHONPATH
+            '';
+          };
+      });
+}
